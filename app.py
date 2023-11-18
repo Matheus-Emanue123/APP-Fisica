006503e32b84523749959b0e09ba7f4a4e42b17d
Matheus Emanuel <memanuel643@gmail.com>
2023-11-18 17:28:43 -0300

    Mudança no Layout do App

--- app.py
+++ app.py
@@ -1,126 +1,107 @@
 import tkinter as tk
 from tkinter import ttk
-import matplotlib.pyplot as plt
-import math
+from matplotlib import pyplot as plt
 import numpy as np
-from matplotlib.figure import Figure
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-from matplotlib.animation import FuncAnimation
+import tkinter as tk
+import numpy as np
+from PIL import Image, ImageTk
 
 class PhysicsGraphApp:
-    
+
     def __init__(self, root):
+
         self.root = root
-        self.root.title("Aplicativo de Gráficos de Física")
-        self.root.configure(bg='white') 
+        self.root.title("Physics Calculations")
 
-        self.label = ttk.Label(root, text="Escolha o tipo de movimento:")
-        self.label.pack()
+        tab_control = ttk.Notebook(self.root)
 
-        self.options = ["MRU", "MRUV", "MCU", "MCUV"]
-        self.choice = tk.StringVar()
-        self.choice.set(self.options[0])
+        mru_tab = ttk.Frame(tab_control)
+        tab_control.add(mru_tab, text='MRU')
+        self.setup_mru_tab(mru_tab)
 
-        self.option_menu = ttk.OptionMenu(root, self.choice, *self.options)
-        self.option_menu.pack()
+        mruv_tab = ttk.Frame(tab_control)
+        tab_control.add(mruv_tab, text='MRUV')
+        self.setup_mruv_tab(mruv_tab)
 
-        self.button = ttk.Button(root, text="Abrir", command=self.open_graph_window, style="TButton")
-        self.button.pack()
+        mcu_tab = ttk.Frame(tab_control)
+        tab_control.add(mcu_tab, text='MCU')
+        self.setup_mcu_tab(mcu_tab)
 
+        mcuv_tab = ttk.Frame(tab_control)
+        tab_control.add(mcuv_tab, text='MCUV')
+        self.setup_mcuv_tab(mcuv_tab)
 
-    def open_graph_window(self):
-        choice = self.choice.get()
-        if choice == "MRU":
-            self.open_mru_window()
-        elif choice == "MRUV":
-            self.open_mruv_window()
-        elif choice == "MCU":
-            self.open_mcu_window()
-        elif choice == "MCUV":
-            self.open_mcuv_window()
+        tab_control.pack(expand=1, fill='both')
 
-    def open_mru_window(self):
+    def setup_mru_tab(self, tab):
 
-        mru_window = tk.Toplevel(self.root)
-        mru_window.title("MRU")
-
-        velocidade_label = ttk.Label(mru_window, text="Digite a velocidade do MRU:")
+        velocidade_label = ttk.Label(tab, text="Digite a velocidade do MRU:")
         velocidade_label.pack()
-        velocidade_entry = ttk.Entry(mru_window)
+        velocidade_entry = ttk.Entry(tab)
         velocidade_entry.pack()
 
-
-        inicial_label = ttk.Label(mru_window, text="Digite a posição inicial:")
+        inicial_label = ttk.Label(tab, text="Digite a posição inicial:")
         inicial_label.pack()
-        inicial_entry = ttk.Entry(mru_window)
+        inicial_entry = ttk.Entry(tab)
         inicial_entry.pack()
 
-        plot_button = ttk.Button(mru_window, text="Plotar Gráfico", command=lambda: self.plot_mru(float(velocidade_entry.get()), np.arange(1.0, 51.0, 0.1).tolist(), float(inicial_entry.get())))
+        plot_button = ttk.Button(tab, text="Plotar Gráfico", command=lambda: self.plot_mru(float(velocidade_entry.get()), np.arange(1.0, 51.0, 0.1).tolist(), float(inicial_entry.get())))
         plot_button.pack()
 
-    def open_mruv_window(self):
+    def setup_mruv_tab(self, tab):
 
-        mruv_window = tk.Toplevel(self.root)
-        mruv_window.title("MRUV")
-
-        velocidade_inicial_label = ttk.Label(mruv_window, text="Digite a velocidade inicial do MRUV:")
+        velocidade_inicial_label = ttk.Label(tab, text="Digite a velocidade inicial:")
         velocidade_inicial_label.pack()
-        velocidade_inicial_entry = ttk.Entry(mruv_window)
+        velocidade_inicial_entry = ttk.Entry(tab)
         velocidade_inicial_entry.pack()
 
-        aceleracao_label = ttk.Label(mruv_window, text="Digite a aceleração do MRUV:")
+        aceleracao_label = ttk.Label(tab, text="Digite a aceleração:")
         aceleracao_label.pack()
-        aceleracao_entry = ttk.Entry(mruv_window)
+        aceleracao_entry = ttk.Entry(tab)
         aceleracao_entry.pack()
 
-        inicial_label = ttk.Label(mruv_window, text="Digite a posição inicial:")
+        inicial_label = ttk.Label(tab, text="Digite a posição inicial:")
         inicial_label.pack()
-        inicial_entry = ttk.Entry(mruv_window)
+        inicial_entry = ttk.Entry(tab)
         inicial_entry.pack()
 
-
-        plot_button = ttk.Button(mruv_window, text="Plotar Gráfico", command=lambda: self.plot_mruv(float(velocidade_inicial_entry.get()), float(aceleracao_entry.get()), np.arange(1.0, 51.0, 0.1).tolist(), float(inicial_entry.get())))
+        plot_button = ttk.Button(tab, text="Plotar Gráfico", command=lambda: self.plot_mruv(float(velocidade_inicial_entry.get()), float(aceleracao_entry.get()), np.arange(1.0, 51.0, 0.1).tolist(), float(inicial_entry.get())))
         plot_button.pack()
 
-    def open_mcu_window(self):
-        mcu_window = tk.Toplevel(self.root)
-        mcu_window.title("MCU")
+    def setup_mcu_tab(self, tab):
 
-        velocidade_linear_label = ttk.Label(mcu_window, text= "Digite a velocidade linear do MCU:")
+        velocidade_linear_label = ttk.Label(tab, text= "Digite a velocidade linear do MCU:")
         velocidade_linear_label.pack()
-        velocidade_linear_entry = ttk.Entry(mcu_window)
+        velocidade_linear_entry = ttk.Entry(tab)
         velocidade_linear_entry.pack()
 
-        raio_label = ttk.Label(mcu_window, text="Digite o raio do MCU:")
+        raio_label = ttk.Label(tab, text="Digite o raio do MCU:")
         raio_label.pack()
-        raio_entry = ttk.Entry(mcu_window)
+        raio_entry = ttk.Entry(tab)
         raio_entry.pack()
 
-        plot_button = ttk.Button(mcu_window, text="Plotar Gráfico", command=lambda: self.plot_mcu(float(velocidade_linear_entry.get()), float(raio_entry.get()), np.arange(1.0, 51.0, 0.1).tolist()))
+        plot_button = ttk.Button(tab, text="Plotar Gráfico", command=lambda: self.plot_mcu(float(velocidade_linear_entry.get()), float(raio_entry.get()), np.arange(1.0, 51.0, 0.1).tolist()))
         plot_button.pack()
 
-    def open_mcuv_window(self):
-        mcuv_window = tk.Toplevel(self.root)
-        mcuv_window.title("MCUV")
+    def setup_mcuv_tab(self, tab):
 
-        velocidade_angular_inicial_label = ttk.Label(mcuv_window, text="Digite a velocidade linear inicial do MCUV:")
+        velocidade_angular_inicial_label = ttk.Label(tab, text="Digite a velocidade linear inicial do MCUV:")
         velocidade_angular_inicial_label.pack()
-        velocidade_angular_inicial_entry = ttk.Entry(mcuv_window)
+        velocidade_angular_inicial_entry = ttk.Entry(tab)
         velocidade_angular_inicial_entry.pack()
 
-        aceleracao_angular_label = ttk.Label(mcuv_window, text="Digite a aceleração linear do MCUV:")
+        aceleracao_angular_label = ttk.Label(tab, text="Digite a aceleração linear do MCUV:")
         aceleracao_angular_label.pack()
-        aceleracao_angular_entry = ttk.Entry(mcuv_window)
+        aceleracao_angular_entry = ttk.Entry(tab)
         aceleracao_angular_entry.pack()
 
-        raio_label = ttk.Label(mcuv_window, text="Digite o raio do MCUV:")
+        raio_label = ttk.Label(tab, text="Digite o raio do MCUV:")
         raio_label.pack()
-        raio_entry = ttk.Entry(mcuv_window)
+        raio_entry = ttk.Entry(tab)
         raio_entry.pack()
 
-        plot_button = ttk.Button(mcuv_window, text="Plotar Gráfico", command=lambda: self.plot_mcuv(float(velocidade_angular_inicial_entry.get()), float(aceleracao_angular_entry.get()), float(raio_entry.get()), np.arange(1.0, 51.0, 0.1).tolist()))
+        plot_button = ttk.Button(tab, text="Plotar Gráfico", command=lambda: self.plot_mcuv(float(velocidade_angular_inicial_entry.get()), float(aceleracao_angular_entry.get()), float(raio_entry.get()), np.arange(1.0, 51.0, 0.1).tolist()))
         plot_button.pack()
-
     
     def plot_mru(self, velocidade, tempo, inicial):
         posicoes = [inicial + velocidade * t for t in tempo]
@@ -185,7 +166,6 @@ class PhysicsGraphApp:
         plt.legend()
 
         plt.tight_layout()
-        plt.show()
 
 root = tk.Tk()
 app = PhysicsGraphApp(root)
